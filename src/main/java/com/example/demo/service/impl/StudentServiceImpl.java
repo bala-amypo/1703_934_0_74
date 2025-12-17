431fc3a2ab4fadc5c793723ac20aa25341a54c60
bala-amypo <bala@amypo.in>
2025-12-17 18:02:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -25,12 +25,11 @@ public class StudentServiceImpl implements StudentService {
     public List<Student> getAllStudents() {
         return strepo.findAll();
     }
-    @override
-    public Student update(Student st){
-        return strepo.save(st);
-    }
 
-    
+    @Override
+    public Optional<Student> getOneStudent(Long id) {
+        return strepo.findById(id);
+    }
 
     @Override
     public void deleteStudent(Long id) {
